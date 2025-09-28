a448afbf0d6860f6f820bd704964c92c40ccd7a6
RomainPeter <romain.peter@gmail.com>
2025-09-28 10:00:59 +0200

    chore: Clean up after merge - remove duplicate files

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Discovery Engine 2-Cat
-Orchestrateur 2-cat, AE/CEGIS, e-graphs, bandit/MCTS, domain adapters, benchmarks.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Discovery Engine Team"
-__description__ = "Orchestrateur 2-cat, AE/CEGIS, e-graphs, bandit/MCTS, domain adapters, benchmarks"
-
